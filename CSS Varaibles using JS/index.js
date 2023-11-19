cb0d108c91b988ef382f65a839b8aac06cb68d3d
imminent-byte <imminent11225@gmail.com>
2023-11-19 21:36:39 +0300

    add more comments

--- CSS Varaibles using JS/index.js
+++ CSS Varaibles using JS/index.js
@@ -3,7 +3,9 @@
 const inputs = document.querySelectorAll(".controls input");
 
 function handleUpdate(){
-    const suffix = this.dataset.sizing || ''; // OR nothing because else we will get undefined 
+    // As we are changing just the value there will be no suffix like px vh etc so we do this, we have data attributes in html.
+    const suffix = this.dataset.sizing || ''; // OR nothing as hex code have nothing because else we will get undefined 
+    // Here documentElement gives us the root element and we make change in the CSS root
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
