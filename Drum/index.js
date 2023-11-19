4086e9c629379f2bd38730a6876b6e566e174c94
imminent-byte <imminent11225@gmail.com>
2023-11-19 18:18:01 +0300

    Version_1

---
+++ Drum/index.js
@@ -0,0 +1,30 @@
+const divItems = document.querySelectorAll('.key');
+divItems.forEach(item => {
+    item.addEventListener('click', function(e) {
+        console.log(e);
+    })
+})
+
+//keyboard functionality
+window.addEventListener("keydown", function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if(!audio) return;
+
+    audio.currentTime=0;
+    audio.play();
+    
+    key.classList.add('playing');
+});
+
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+    key.addEventListener('transitionend', function(e) {
+        if(e.propertyName !== 'transform') return;
+
+
+        this.classList.remove('playing');
+    });
+});
