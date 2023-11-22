1c48fabb8b723fd08cfb7cb0a9e8d3a8679a6a91
imminent-byte <imminent11225@gmail.com>
2023-11-22 22:03:06 +0300

    Speech Recognition

---
+++ Speech recognition/index.js
@@ -0,0 +1,26 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+// this will give us the words we speak as we speak them rather than when we have finished speaking
+recognition.interimResults = true;
+
+
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+    const transcript = [...e.results]
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join(" ");
+        
+        p.textContent = transcript;
+        if(e.results[0].isFinal){
+            p = document.createElement('p');
+            words.appendChild(p);
+        }
+});
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
